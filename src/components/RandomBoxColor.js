07b9844f4a44a440e4dfb2264fddcce61c692aef
rev bar <revital.jer@gmail.com>
2020-12-30 18:17:58 +0200

    render box with random color every refresh

--- src/components/RandomBoxColor.js
+++ src/components/RandomBoxColor.js
@@ -5,38 +5,41 @@ import './RandomBoxColor.css';
 class RandomBoxColor extends React.Component{
 
     constructor (props){
-        super (props)
+        super (props);
 
         this.state = {
 
-            color : "red"
+            color : this.getRandomColor()
         }
-
-
     }
 
-render () {
-    //converting data to presentation:
-    //convert color state to style 
-
-    const {color} = this.state
-    const boxStyle = {
-        backgroundColor: color
+       
+    getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            const index = Math.floor(Math.random() * 16);
+            color += letters[index];
+        }
+        return color;
     }
 
-    return(
-        <>
-            <div className="box" style= {boxStyle}> </div>
-        </>
+
+    render () {
+        //converting data to presentation:
+        //convert color state to style 
+
+        const {color} = this.state
+        const boxStyle = {backgroundColor: color}
+
+        return(
+            <>
+                <div className="box" style= {boxStyle}> </div>
+            </>
 
 
-    )
+        )
+    }
 }
-
-
-}
-
-
-
 
 export default RandomBoxColor;
