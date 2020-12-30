4c3a082a7d38d0e7daadb86e04aab126827366f7
rev bar <revital.jer@gmail.com>
2020-12-31 01:29:44 +0200

    adding states to the radio buttens grou+ rendernig no to q2/3 if q1 ===no

--- src/components/ProgramTest.js
+++ src/components/ProgramTest.js
@@ -4,92 +4,60 @@ import './ProgramTest.css';
 
 
 function ProgramTest () {
+
     const [checkedState, setCheckedState] = useState();
+    const [question1, setQuestion1] = useState();
+    const [question2, setQuestion2] = useState();
+    const [question3, setQuestion3] = useState();
  
-      <>
-    <div>
-        <label>Have you Programmed before?</label>
-        <Container className="text-center">
-            <label>Yes</label>   
-            <InputGroup.Radio name= "Programmed before"  >
-            </InputGroup.Radio>
-            <label>No</label>   
-            <InputGroup.Radio name= "Programmed before" onClick = {() => setCheckedState(true) } >
-            </InputGroup.Radio>
-            <label>Partial</label>   
-            <InputGroup.Radio name= "Programmed before"  >
-            </InputGroup.Radio>
-        </Container>
-
-    </div>
-    <div>
-        <label>Have you Programmed in Java?</label>
-        <Container className="text-center">
-            <label>Yes</label>   
-            <InputGroup.Radio   >
-            </InputGroup.Radio>
-            <label>No</label>   
-            <InputGroup.Radio checked={checkedState} >
-            </InputGroup.Radio>
-            <label>Partial</label>   
-            <InputGroup.Radio   >
-            </InputGroup.Radio>
-        </Container>
-
-    </div>
-    <div>
-        <label>Have you Programmed in Pyton?</label>
-        <Container className="text-center">
-            <label>Yes</label>   
-            <InputGroup.Radio   >
-            </InputGroup.Radio>
-            <label>No</label>   
-            <InputGroup.Radio checked={checkedState} >
-            </InputGroup.Radio>
-            <label>Partial</label>   
-            <InputGroup.Radio   >
-            </InputGroup.Radio>
-        </Container>
-    </div>
+    useEffect(() => {
+      if(question1 === 'option2'){
+        setQuestion2('option2')  
+      } else{
+        setQuestion2() 
+      }
+      
+        
+      }, [question1])
 
 
+    return(
+        <>
+            <div value = {question1}  >
+                <label>Have you Programmed before?</label>
+                <Container className="text-center">
+                    <label>Yes</label>   
+                    <InputGroup.Radio value="option1" checked={question1 === 'option1'} onChange= {(e) => setQuestion1(e.target.value)}  >
+                    </InputGroup.Radio>
+                    <label>No</label>   
+                    <InputGroup.Radio  value="option2" checked={question1 === 'option2'} onChange= {(e) => setQuestion1(e.target.value)} >
+                    </InputGroup.Radio>
+                    <label>Partial</label>   
+                    <InputGroup.Radio  value="option3" checked={question1 === 'option3'} onChange= {(e) => setQuestion1(e.target.value)}  >
+                    </InputGroup.Radio>
+                </Container>
+            </div>
+            <div value = {question2}  >
+                <label>Have you Programmed in java??</label>
+                <Container className="text-center">
+                    <label>Yes</label>   
+                    <InputGroup.Radio value="option1" checked={question2 === 'option1'} onChange= {(e) => setQuestion2(e.target.value)}  >
+                    </InputGroup.Radio>
+                    <label>No</label>   
+                    <InputGroup.Radio  value="option2" checked={question2 === 'option2'} onChange= {(e) => setQuestion2(e.target.value)} >
+                    </InputGroup.Radio>
+                    <label>Partial</label>   
+                    <InputGroup.Radio  value="option3" checked={question2 === 'option3'} onChange= {(e) => setQuestion2(e.target.value)}  >
+                    </InputGroup.Radio>
+                </Container>
+            </div>
+        </>    
+    )
+     
 
 
-
-
-
-</>
-
-  )
 }
-
-
 
 
 export default ProgramTest;
 
-    {/* <label>Have you Programmed before?</label>
-    <Container className="text-center">
-         <label>Yes</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-        <label>No</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-        <label>Partial</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-    </Container>
-
-    <label>Have you Programmed in Java?</label>
-    <Container className="text-center">
-         <label>Yes</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-        <label>No</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-        <label>Partial</label>   
-         <InputGroup.Radio aria-label="Radio button for following text input" >
-        </InputGroup.Radio>
-    </Container> */}
